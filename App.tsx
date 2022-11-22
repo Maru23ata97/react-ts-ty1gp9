6a03b0bd2fbd6a409a3e3b1d852dc3fa73caaea6
Maru23ata97 <118214748+Maru23ata97@users.noreply.github.com>
2022-11-21 15:00:21 -1000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import './style.css';
+import Card from './src/Card.tsx';
+export default function App() {
+  const [data, setData] = React.useState();
+  const [name, setName] = React.useState();
+
+  React.useEffect(() => {
+    setName('Bonjour');
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  return (
+    <div>
+      {name}
+      <Card />
+      <Card />
+      <Card />
+
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-ty1gp9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ty1gp9)
